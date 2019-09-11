96bf26a412cb0eb5cb38aa610a49fb77d5bcc1b9
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-09-11 09:41:23 +0200

    Licznik Tekstu

---
+++ src/pl/bb/javastart/servlet/TextCounterServlet.java
@@ -0,0 +1,45 @@
+package pl.bb.javastart.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+
+@WebServlet("/counter")
+public class TextCounterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        String tekst = request.getParameter("tekst");
+        int character = 0;
+
+        for (int i = 0; i < tekst.length(); i++) {
+            character++;
+        }
+
+        String[] words = tekst.split(" ");
+        byte[] byteText = tekst.getBytes();
+        byte[] rewerstext = new byte[byteText.length];
+
+        for (int i = 0; i < byteText.length; i++) {
+            rewerstext[i] = byteText[byteText.length - i - 1];
+        }
+
+        boolean palindrom = Arrays.equals(byteText, rewerstext);
+
+        writer.println("Ilość słów: " + words.length);
+        writer.println("Ilość znaków: " + character);
+        writer.println("Ilość znaków bez spacji: " + (character - (words.length - 1)));
+        writer.println("Palindrom: " + palindrom);
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dell
+  Date: 11.09.2019
+  Time: 09:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>$Title$</title>
+    </head>
+    <body>
+        <h1>Przelicznik tekstu</h1>
+
+    <form method="post" action="counter">
+        <input type="text" name="tekst"><br/>
+        <input type="submit" value="Wyślij"><br/>
+    </form>
+    </body>
+</html>
